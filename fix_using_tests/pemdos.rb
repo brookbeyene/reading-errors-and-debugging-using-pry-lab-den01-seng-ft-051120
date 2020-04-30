f2ac170f7c50bc75c202f3f615895979bddeaab5
Brook Beyene <brookbeyene37@gmail.com>
2020-04-30 22:55:02 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,12 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    newS = 10.times{"s"}
+    newS.to_i + string
   else
     string
+    binding.pry
   end
 end
